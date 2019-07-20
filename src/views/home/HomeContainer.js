e8825e099608002ca0c8ed6ae690e563bed2c1f3
Indrek Lasn <lasnindrek@gmail.com>
2019-07-20 12:45:37 +0300

    eslint

---
+++ src/views/home/HomeContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+export default () => (
+  <Query
+    query={gql`
+      query {
+        getProductById(id: "5d32d50c7c213e5998eed883") {
+          name
+          url
+          id
+          tags
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      console.log(loading);
+      console.log(data);
+      return 1;
+    }}
+  </Query>
+);
