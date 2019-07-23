3c94b724b46f51171514381f796268c28c711b56
Indrek Lasn <lasnindrek@gmail.com>
2019-07-23 16:26:42 +0300

    poll interval

--- src/views/home/ShowAllProducts.js
+++ src/views/home/ShowAllProducts.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo';
 import Product from './Product';
 
 const ShowAllProducts = () => (
-	<Query query={getAllProducts}>
+	<Query query={getAllProducts} pollInterval={60 * 60}>
 		{({ loading, error, data }) => {
 			if (loading) return <span>...loading</span>;
 			if (error) return `${error}`;
